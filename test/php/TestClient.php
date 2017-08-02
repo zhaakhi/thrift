d2eb4526f3b90359e4b7f3865ac412cc153bc07f
Håkon Hitland <hakon.hitland@zedge.net>
2017-08-02 13:24:33 +0200

    Store return codes as class constants instead of defines

--- test/php/TestClient.php
+++ test/php/TestClient.php
@@ -124,15 +124,17 @@ $start = microtime(true);
 
 $testRunner = new TestRunner($testClient);
 
-define('ERR_BASETYPES', 1);
-define('ERR_STRUCTS', 2);
-define('ERR_CONTAINERS', 4);
-define('ERR_EXCEPTIONS', 8);
-define('ERR_UNKNOWN', 64);
-
+// Return codes defined in test/README.md, the return code will an OR of these
+class ErrVal {
+    const BASETYPES = 1;
+    const STRUCTS = 2;
+    const CONTAINERS = 4;
+    const EXCEPTIONS = 8;
+    const UNKNOWN = 64;
+}
 
 // VOID TEST
-$testRunner->run('testVoid()', ERR_BASETYPES, function($testClient) {
+$testRunner->run('testVoid()', ErrVal::BASETYPES, function($testClient) {
     $testClient->testVoid();
 });
 
@@ -173,30 +175,30 @@ $xtruct->string_thing = "Zero";
 $xtruct->byte_thing = 1;
 $xtruct->i32_thing = -3;
 $xtruct->i64_thing = -5;
-$testRunner->roundtripNonStrict('testStruct', $xtruct, ERR_STRUCTS);
+$testRunner->roundtripNonStrict('testStruct', $xtruct, ErrVal::STRUCTS);
 
 // NESTED STRUCT TEST
 $xtruct2 = new \ThriftTest\Xtruct2();
 $xtruct2->byte_thing = 1;
 $xtruct2->struct_thing = $xtruct;
 $xtruct2->i32_thing = 5;
-$testRunner->roundtripNonStrict('testNest', $xtruct2, ERR_STRUCTS);
+$testRunner->roundtripNonStrict('testNest', $xtruct2, ErrVal::STRUCTS);
 
 // MAP TEST
 $mapout = array();
 for ($i = 0; $i < 5; ++$i) {
   $mapout[$i] = $i-10;
 }
-$testRunner->roundtripIgnoreKeyOrder('testMap', $mapout, ERR_CONTAINERS);
+$testRunner->roundtripIgnoreKeyOrder('testMap', $mapout, ErrVal::CONTAINERS);
 
 $mapout = array();
 for ($i = 0; $i < 11; $i++) {
     $mapout["key$i"] = "val$i";
 }
-$testRunner->roundtripIgnoreKeyOrder('testStringMap', $mapout, ERR_CONTAINERS);
+$testRunner->roundtripIgnoreKeyOrder('testStringMap', $mapout, ErrVal::CONTAINERS);
 
 // SET TEST
-$testRunner->run('testSet', ERR_CONTAINERS, function($testClient) {
+$testRunner->run('testSet', ErrVal::CONTAINERS, function($testClient) {
     $setout = array();
     for ($i = -2; $i < 3; ++$i) {
         $setout[$i]= true;
@@ -218,20 +220,20 @@ $listout = array();
 for ($i = -2; $i < 3; ++$i) {
   $listout[]= $i;
 }
-$testRunner->roundtrip('testList', $listout, ERR_CONTAINERS);
+$testRunner->roundtrip('testList', $listout, ErrVal::CONTAINERS);
 
 // ENUM TEST
-$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::ONE, ERR_STRUCTS);
-$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::TWO, ERR_STRUCTS);
-$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::THREE, ERR_STRUCTS);
-$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::FIVE, ERR_STRUCTS);
-$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::EIGHT, ERR_STRUCTS);
+$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::ONE, ErrVal::STRUCTS);
+$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::TWO, ErrVal::STRUCTS);
+$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::THREE, ErrVal::STRUCTS);
+$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::FIVE, ErrVal::STRUCTS);
+$testRunner->roundtrip('testEnum', \ThriftTest\Numberz::EIGHT, ErrVal::STRUCTS);
 
 // TYPEDEF TEST
-$testRunner->roundtrip('testTypedef', 309858235082523, ERR_STRUCTS);
+$testRunner->roundtrip('testTypedef', 309858235082523, ErrVal::STRUCTS);
 
 // NESTED MAP TEST
-$testRunner->run('testMapMap(1)', ERR_CONTAINERS, function($testClient) {
+$testRunner->run('testMapMap(1)', ErrVal::CONTAINERS, function($testClient) {
     $mm = $testClient->testMapMap(1);
     echo " = " . prettyFormat($mm) . "\n";
     $expected_mm = [
@@ -246,7 +248,7 @@ $testRunner->run('testMapMap(1)', ERR_CONTAINERS, function($testClient) {
 });
 
 // INSANITY TEST
-$testRunner->run('testInsanity()', ERR_STRUCTS, function($testClient) {
+$testRunner->run('testInsanity()', ErrVal::STRUCTS, function($testClient) {
     $insane = new \ThriftTest\Insanity();
     $insane->userMap[\ThriftTest\Numberz::FIVE] = 5000;
     $truck = new \ThriftTest\Xtruct();
@@ -262,7 +264,7 @@ $testRunner->run('testInsanity()', ERR_STRUCTS, function($testClient) {
 });
 
 // EXCEPTION TEST
-$testRunner->run("testException('Xception')", ERR_EXCEPTIONS, function($testClient) {
+$testRunner->run("testException('Xception')", ErrVal::EXCEPTIONS, function($testClient) {
     try {
         $testClient->testException('Xception');
         throw new \RuntimeException("Should have thrown exception");
@@ -299,7 +301,7 @@ echo "Total time: $elp ms\n";
 
 // REGRESSION TESTS
 if ($protocol instanceof TBinaryProtocolAccelerated) {
-    $testRunner->run('THRIFT-3984 do not double free strings', ERR_UNKNOWN, function($testClient) use ($protocol) {
+    $testRunner->run('THRIFT-3984 do not double free strings', ErrVal::UNKNOWN, function($testClient) use ($protocol) {
         // Regression check: check that method name is not double-freed
         // Method name should not be an interned string.
         $method_name = "Void";
@@ -336,7 +338,7 @@ class TestRunner {
      */
     public function run($testName, $errorCode, $testCode) {
         if (!$errorCode) {
-            $errorCode = ERR_UNKNOWN;
+            $errorCode = ErrVal::UNKNOWN;
         }
         try {
             echo "#$testName\n";
@@ -351,7 +353,7 @@ class TestRunner {
     /**
      * Convenience method for checking roundtrip of a single value
      */
-    private function roundtripWithCheck($method, $value, $errorCode=ERR_BASETYPES, $checkFun) {
+    private function roundtripWithCheck($method, $value, $errorCode=ErrVal::BASETYPES, $checkFun) {
         $value_str = prettyFormat($value);
         $this->run("$method($value_str)", $errorCode, function($testClient) use ($method, $value, $checkFun) {
             $result = $testClient->$method($value);
@@ -363,7 +365,7 @@ class TestRunner {
     /**
      * Convenience method for checking roundtrip of a single value
      */
-    public function roundtrip($method, $value, $errorCode=ERR_BASETYPES) {
+    public function roundtrip($method, $value, $errorCode=ErrVal::BASETYPES) {
         $this->roundtripWithCheck($method, $value, $errorCode, function($a, $b) {
             assertSame($a, $b);
         });
@@ -372,7 +374,7 @@ class TestRunner {
     /**
      * Convenience method for checking roundtrip of a single value, with non-strict comparison
      */
-    public function roundtripNonStrict($method, $value, $errorCode=ERR_BASETYPES) {
+    public function roundtripNonStrict($method, $value, $errorCode=ErrVal::BASETYPES) {
         $this->roundtripWithCheck($method, $value, $errorCode, function($a, $b) {
             assertEqual($a, $b);
         });
@@ -381,7 +383,7 @@ class TestRunner {
     /**
      * Convenience method for checking roundtrip of a single value, sorting on key before comparison
      */
-    public function roundtripIgnoreKeyOrder($method, $value, $errorCode=ERR_BASETYPES) {
+    public function roundtripIgnoreKeyOrder($method, $value, $errorCode=ErrVal::BASETYPES) {
         $this->roundtripWithCheck($method, $value, $errorCode, function($a, $b) {
             ksort($a);
             ksort($b);
